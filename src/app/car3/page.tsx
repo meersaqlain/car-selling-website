1cd3f586b13e1f54fa6f5f3b86a3f494922a0bde
meersaqlain <meersaqlain055@gmail.com>
2024-10-25 22:35:19 +0500

    text-coloring

--- src/app/car3/page.tsx
+++ src/app/car3/page.tsx
@@ -19,8 +19,8 @@ export default function car2(){
                 <button className="bg-blue-700 py-3 px-6 rounded-lg border border-white">Visit Place</button>
                 <button className="bg-blue-700 py-3 px-6 rounded-lg border border-white">Car Inspection</button>
             </div>
-            <h1 className="flex justify-center text-2xl font-semibold m-10">Vehicle Description</h1>
-            <div className="flex justify-center m-10">
+            <h1 className="flex justify-center text-2xl font-semibold m-10 text-white">Vehicle Description</h1>
+            <div className="flex justify-center m-10 text-white">
                 <p><b>Number of Doors</b> 4  &emsp; <b>Engine</b> 1800 CC &emsp; <b>Driven</b> 9500 KM &emsp; <b>Suspention Type</b>: Soft Suspention
                 <br/>
                 <b>Avg</b> 13 km per ltr &emsp; <b>Transmission</b> Automatic &emsp; <b>Fuel Type</b> High Octane </p>
